d9ec5e52cb99e6bf0f9e367fe1e06131f1cd8684
BritishBambi <josefjakubiak@gmail.com>
2022-08-16 22:44:23 +0000

    Add default placeholder image to new Products

--- products/models.py
+++ products/models.py
@@ -24,7 +24,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True, default='placeholder.jpg')
 
 
 RATE_CHOICES = [
