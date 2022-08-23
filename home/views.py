161f531748544eb08d6a78ef913b2b2e9ae40466
BritishBambi <josefjakubiak@gmail.com>
2022-08-23 01:02:10 +0000

    Add about us template and view

--- home/views.py
+++ home/views.py
@@ -6,3 +6,8 @@ from django.shortcuts import render
 def index(request):
     """ A Simple view to render the home screen """
     return render(request, 'home/index.html')
+
+
+def about_us(request):
+    """ A Simple view to render the about us page """
+    return render(request, 'home/about_us.html')
