635d35f2ef1838a17b63a50cdd5619b5ec584e4d
BritishBambi <josefjakubiak@gmail.com>
2022-08-23 15:05:06 +0000

    Customise toast apperances and clean up navigation menus

--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render,  redirect, reverse, HttpResponse
+from django.shortcuts import render,  redirect, reverse, HttpResponse, get_object_or_404
+from django.contrib import messages
 
+from products.models import Product
 # Create your views here.
 
 
@@ -10,6 +12,7 @@ def view_bag(request):
 
 def add_to_bag(request, item_id):
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
@@ -18,6 +21,7 @@ def add_to_bag(request, item_id):
         bag[item_id] += quantity
     else:
         bag[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
@@ -31,6 +35,7 @@ def adjust_bag(request, item_id):
 
     if quantity > 0:
         bag[item_id] = quantity
+        messages.success(request, 'Quantity updated')
     else:
         bag.pop(item_id)
 
@@ -43,6 +48,7 @@ def remove_from_bag(request, item_id):
 
     bag = request.session.get('bag', {})
     bag.pop(item_id)
+    messages.success(request, 'Item removed from your bag')
 
     request.session['bag'] = bag
     return HttpResponse(status=200)
