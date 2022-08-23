b3eb4bf8cbc2a55d83f5e0618d5bc166b1a8675e
BritishBambi <josefjakubiak@gmail.com>
2022-08-23 16:34:32 +0000

    Begin testing views with setup user account

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,37 @@
+import os
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+API_KEY = os.environ.get("API_KEY")
 
 # Create your tests here.
+
+
+class TestViews(TestCase):
+    """
+
+    """
+    def setUp(self):
+        """ Create test login user and create Profile entry"""
+        username = "jojo"
+        pswd = "Trials"
+        user_model = get_user_model()
+        self.user = user_model.objects.create_user(username=username,
+                                                   password=pswd,
+                                                   is_superuser=True)
+        logged_in = self.client.login(username=username, password=pswd)
+
+        self.assertTrue(logged_in)
+
+    def test_home_page(self):
+        """ TC1 - Test home view renders correct page """
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/index.html')
+
+    def test_about_us_page(self):
+        """ TC2 - Test about us view renders the correct page"""
+        response = self.client.get('/about_us/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/about_us.html')
