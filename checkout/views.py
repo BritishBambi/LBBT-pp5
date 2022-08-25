1c954dc8e7a238bc599f18ec72ee02a070e5bb7c
BritishBambi <josefjakubiak@gmail.com>
2022-08-25 00:20:41 +0000

    add base stripe elements to checkout

--- checkout/views.py
+++ checkout/views.py
@@ -9,11 +9,13 @@ def checkout(request):
     if not bag:
         messages.error(request, "There's nothing currently in your bag")
         return redirect(reverse('products'))
-    
+
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51LaTeTKqUpIuVKpWHTwUCm7ZIvtiGeKYIwu5EVGiZZ8x7a6qFBtmOtJRjCMJu15p497jZIHxaUhNCemQFKQQFI1X006t405W7I',
+        'client_test_secret': 'test_secret'
     }
 
     return render(request, template, context)
