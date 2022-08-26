63a2880815dd39fe89039d6a065af239f6dbc66c
BritishBambi <josefjakubiak@gmail.com>
2022-08-26 20:11:12 +0000

    Add edit product function

--- products/urls.py
+++ products/urls.py
@@ -4,8 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.all_products, name='products'),
     path('<int:product_id>/', views.product_detail, name='product_detail'),
-    path('<int:product_id>/rate/', views.review_product, name='review_product'),
+    path('<int:product_id>/rate/', views.review_product,
+         name='review_product'),
     path('<int:product_id>/review/<username>/', views.view_review,
          name='view_review'),
     path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
 ]
