04afa387e3c18afe472eaf857beea773c9cb259d
BritishBambi <josefjakubiak@gmail.com>
2022-08-26 18:32:48 +0000

    Add product management form

--- LBBT/settings.py
+++ LBBT/settings.py
@@ -119,7 +119,7 @@ LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-if 'DEVELOPMENT' in os.environ:
+if os.environ.get("DEVELOPMENT"):
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
--- products/forms.py
+++ products/forms.py
@@ -1,11 +1,11 @@
 from django import forms
-from products.models import Review, RATE_CHOICES
+from products.models import Review, RATE_CHOICES, Product, Category
 
 
 class RateForm(forms.ModelForm):
     """
     Form used to submit a product review. Takes a text area for inputing the
-    written review as well as a choice field of scores from 1-10
+    written review as well as a choice field of scores from 1-5
     """
     text = forms.CharField(widget=forms.Textarea(
         attrs={'class': 'text-black'}), required=False)
@@ -16,3 +16,19 @@ class RateForm(forms.ModelForm):
         """ Gives the text and rate form fields to the Review model """
         model = Review
         fields = ('text', 'rate')
+
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
