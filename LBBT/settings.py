60770de09b839cd6abce14d7791d73b126ba33d0
BritishBambi <josefjakubiak@gmail.com>
2022-08-26 21:32:48 +0000

    Add new smtp settings to fix email on live

--- LBBT/settings.py
+++ LBBT/settings.py
@@ -123,7 +123,7 @@ if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = 'smtp.sendgrid.net'
+    EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_PORT = 587
     EMAIL_USE_TLS = True
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
